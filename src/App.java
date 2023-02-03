fce64bc3d7bdde3360180696ccd845b1ec2f1123
enovak89 <enovak89@mail.ru>
2023-02-03 22:58:54 +0300

    hwObjectsMethods

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void library() {
         Author firstAuthor = new Author("Lev", "Tolstoy");
-        Author secondAuthor = new Author("Fedor", "Dostoyevsky");
+        Author secondAuthor = new Author("Fedor", "Tolstoy");
         Book firstBook = new Book("War & peace", firstAuthor, 1867);
-        Book secondBook = new Book("Idiot", firstAuthor, 1869);
+        Book secondBook = new Book("War & peace", firstAuthor, 1869);
 
 
         firstBook.setYear(1901);
@@ -11,5 +11,12 @@ public class App {
 
         System.out.println("firstBook.getYear() = " + firstBook.getYear());
         System.out.println("secondBook.getYear() = " + secondBook.getYear());
+
+        System.out.println("firstAuthor.toString() = " + firstAuthor.toString());
+        System.out.println("secondAuthor.toString() = " + secondAuthor.toString());
+        System.out.println("firstAuthor.equals(secondAuthor) = " + firstAuthor.equals(secondAuthor));
+
+        System.out.println("firstBook.toString() = " + firstBook.toString());
+        System.out.println("firstBook.equals(secondBook) = " + firstBook.equals(secondBook));
     }
 }
